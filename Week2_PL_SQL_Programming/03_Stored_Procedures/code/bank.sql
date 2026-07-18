52fb1dd5b70c4453738fb332dff8074b0cdea01b
Shiv Kumar <kumarshivaji0837@gmail.com>
2026-07-18 20:11:08 +0530

    Inserting Data into the table

--- Week2_PL_SQL_Programming/03_Stored_Procedures/code/bank.sql
+++ Week2_PL_SQL_Programming/03_Stored_Procedures/code/bank.sql
@@ -55,3 +55,34 @@ CREATE TABLE ErrorLogs (
     ErrorMessage VARCHAR2(255),
     ErrorDate DATE
 );
+
+-- Inserting Data into the Tables 
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (1, 'Jane', TO_DATE('1963-05-15', 'YYYY-MM-DD'), 1000, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (2, 'Jhon Doe', TO_DATE('1990-07-20', 'YYYY-MM-DD'), 1500, SYSDATE);
+
+INSERT INTO Accounts (AccountID, CustomerID, AccountType, Balance, LastModified)
+VALUES (1, 1, 'Savings', 1000, SYSDATE);
+
+INSERT INTO Accounts (AccountID, CustomerID, AccountType, Balance, LastModified)
+VALUES (2, 2, 'Checking', 1500, SYSDATE);
+
+INSERT INTO Transactions (TransactionID, AccountID, TransactionDate, Amount, TransactionType)
+VALUES (1, 1, SYSDATE, 200, 'Deposit');
+
+INSERT INTO Transactions (TransactionID, AccountID, TransactionDate, Amount, TransactionType)
+VALUES (2, 2, SYSDATE, 300, 'Withdrawal');
+
+INSERT INTO Loans (LoanID, CustomerID, LoanAmount, InterestRate, StartDate, EndDate)
+VALUES (1, 1, 5000, 5, SYSDATE, ADD_MONTHS(SYSDATE, 60));
+
+INSERT INTO Loans (LoanID, CustomerID, LoanAmount, InterestRate, StartDate, EndDate)
+VALUES (2, 2, 10000, 5, SYSDATE, SYSDATE + 25);
+
+INSERT INTO Employees (EmployeeID, Name, Position, Salary, Department, HireDate)
+VALUES (1, 'Alice Johnson', 'Manager', 70000, 'HR', TO_DATE('2015-06-15', 'YYYY-MM-DD'));
+
+INSERT INTO Employees (EmployeeID, Name, Position, Salary, Department, HireDate)
+VALUES (2, 'Bob Brown', 'Developer', 60000, 'IT', TO_DATE('2017-03-20', 'YYYY-MM-DD'));
