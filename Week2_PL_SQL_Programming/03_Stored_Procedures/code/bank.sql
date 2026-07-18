18ec49d69b53342605f1b451d447899b1f371e7b
Shiv Kumar <kumarshivaji0837@gmail.com>
2026-07-18 20:09:01 +0530

    Creating Table Structures

---
+++ Week2_PL_SQL_Programming/03_Stored_Procedures/code/bank.sql
@@ -0,0 +1,50 @@
+-- Enable server output
+SET SERVEROUTPUT ON;
+
+-- Create tables
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    Name VARCHAR2(100),
+    DOB DATE,
+    Balance INT,
+    LastModified DATE,
+    IsVIP CHAR(1)
+);
+
+CREATE TABLE Accounts (
+    AccountID INT PRIMARY KEY,
+    CustomerID INT,
+    AccountType VARCHAR2(20),
+    Balance INT,
+    LastModified DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+CREATE TABLE Transactions (
+    TransactionID INT PRIMARY KEY,
+    AccountID INT,
+    TransactionDate DATE,
+    Amount INT,
+    TransactionType VARCHAR2(10),
+    FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+);
+
+CREATE TABLE Loans (
+    LoanID INT PRIMARY KEY,
+    CustomerID INT,
+    LoanAmount INT,
+    InterestRate INT,
+    StartDate DATE,
+    EndDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    Name VARCHAR2(100),
+    Position VARCHAR2(50),
+    Salary INT,
+    Department VARCHAR2(50),
+    DepartmentID INT,
+    HireDate DATE
+);
