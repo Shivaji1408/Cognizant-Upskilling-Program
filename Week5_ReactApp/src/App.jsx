4c980e2997af9b0f8a5cef4e7a23ec97273744fb
Shiv Kumar <kumarshivaji0837@gmail.com>
2026-07-23 10:08:57 +0530

    Week5 React Application

---
+++ Week5_ReactApp/src/App.jsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import Home from './Components/Home';
+// import About from './Components/About';
+// import Contact from './Components/Contact';
+
+// function App() {
+//   return (
+//     <div>
+//       <Home />
+//       <About />
+//       <Contact />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function App() {
+  return <h1>Student App Working</h1>;
+}
+
+export default App;
