060897f32e5fb178e055245e6c319740363dbf4b
Shiv Kumar <kumarshivaji0837@gmail.com>
2026-07-23 10:24:50 +0530

    Week 5 Student React Application

---
+++ Week5_StudentApp/src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <h2>Welcome to the Home page of Student Management Portal</h2>
+    );
+  }
+}
+
+export default Home;
